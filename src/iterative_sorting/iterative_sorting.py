133703cbbb1abbc94d9b55529137b69c82e70ce8
Eiosx <eiosxenzo@gmail.com>
2020-03-17 23:08:23 -0400

    Finished selection sort and bubble sort problems.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,20 +1,21 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+
+    for i in range(0, len(arr)):
         cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        
+        if i == 0:
+          pass
+        else:
+          while cur_index > 0:
+            if arr[cur_index] < arr[cur_index-1]:
+              smallest_index = arr[cur_index]
+              biggest_index = arr[cur_index-1]
+              arr[cur_index] = biggest_index
+              arr[cur_index-1] = smallest_index
+              cur_index -= 1
+            else:
+              break
 
     return arr
 
@@ -22,8 +23,25 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+  if len(arr) == 0:
     return arr
 
+  swap = True
+
+  while swap:
+    count = 0
+    for i in range(0, len(arr)):
+      if i == (len(arr) - 1):
+        if count == 0:
+          swap = False
+
+      elif arr[i] > arr[i+1]:
+        biggest_index = arr[i]
+        smallest_index = arr[i+1]
+        arr[i] = smallest_index
+        arr[i+1] = biggest_index
+        count += 1
+  return arr
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
